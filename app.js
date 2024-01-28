f784ccf3f2be36175af72d02ec9f83306cd4f77a
Aditya Bansal <110539909+Adityab1179@users.noreply.github.com>
2024-01-28 14:06:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,8 +11,20 @@ tl.from(".content",{
     duration1:1
 })
 tl.from(".images img",{
-    y:100,
+    y:30,
     opacity:0,
     duration:1.5,
     stagger:0.2
+})
+tl.from(".scroll-btn p",{
+    scale:0,
+    opacity:0,
+    y:-20
+
+})
+tl.to(".scroll-btn p",{
+    y:-20,
+    repeat:-1,
+    yoyo:true,
+    duration:.5
 })
